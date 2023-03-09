32ca9a6836209d430d9a1f8110625f95b8e67552
Khanh <dreamproperty.khanh@gmail.com>
2023-03-09 22:52:42 +0700

    reset

--- file2.py
+++ file2.py
@@ -1,10 +1,4 @@
 print("built at 3:40")
-print("fix error during working on new2")
-print("fix error during working on new3")
-print("working on new branch")
-print("fix err again at main")
-
-print("working on new2")
 
 
 
