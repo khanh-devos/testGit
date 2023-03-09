397a0125e458bab30716c5cfb14025bfe9e54901
Khanh <dreamproperty.khanh@gmail.com>
2023-03-09 17:31:45 +0700

    fix error again

--- file2.py
+++ file2.py
@@ -3,4 +3,5 @@ print("this is created at master 4:10")
 print("fix the urgent error OK")
 print("This is created at new1")
 print("This is created at new1 again")
+print("fix the urgent error OK again")
 
