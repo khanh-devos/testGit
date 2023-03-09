9baa126d20fad0d56a03e48936eee17fea955e03
Khanh <dreamproperty.khanh@gmail.com>
2023-03-09 17:43:20 +0700

    reset

--- file2.py
+++ file2.py
@@ -1,7 +1,1 @@
 print("built at 3:40")
-print("this is created at master 4:10")
-print("fix the urgent error OK")
-print("This is created at new1")
-print("This is created at new1 again")
-print("fix the urgent error OK again")
-
