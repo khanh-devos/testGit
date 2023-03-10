f284b9aa55d754b19ae7989b787e865f655f42b5
Khanh <dreamproperty.khanh@gmail.com>
2023-03-10 11:25:39 +0700

    reset

--- file2.py
+++ file2.py
@@ -1,11 +1,4 @@
 print("built at 3:40")
 
-print("there is a problem here => fix the problem")
-
-print("working on D")
-
-print("there is another problem => fix")
-
-
 
 
--- file4.py
+++
@@ -1,3 +0,0 @@
-print("this is file4")
-
-print("there is a problem => fix")
